de05f4a083ae0708b2069e62b65c9a4fe3c02726
elizaveta2711 <mishunina_2002@bk.ru>
2021-12-13 11:57:27 +0300

    Добавьте файлы проекта.

---
+++ AZS/Context/DBContext.cs
@@ -0,0 +1,28 @@
+﻿using AZS.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZS.Context
+{
+    public class DBContext : DbContext
+    {
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Date> Date { get; set; }
+        public DbSet<Station> Station { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ AZS/Model/Station.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZS.Model
+{
+    public class Station
+    {
+        [Key]
+        public int Station_Id { get; set; }
+
+        private string _address;
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                if ((!string.IsNullOrWhiteSpace(value)) && (value.Length <= 100))
+                    _address = value;
+
+            }
+        }
+
+    }
+}
